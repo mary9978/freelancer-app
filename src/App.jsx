a2652b7a2d1ca96fc0464b386395e9fe42ce5deb
maryam rezaei <mari.re9978@gmail.com>
2024-01-08 14:25:38 +0330

    change something

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css'
 import Auth from './pages/Auth'
 import { Route,Routes } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast';
   const queryClient = new QueryClient();
 function App() {
 
 
   return (
     <QueryClientProvider client={queryClient}>
+      <Toaster/>
       <div className="container xl:max-w-screen-xl">
         <Routes>
           <Route path="/auth" element={<Auth />} />
